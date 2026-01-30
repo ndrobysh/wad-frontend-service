51861839919cd66a248a39bb5e47153c95a2befb
nicolai <n.drobyshevski@gmail.com>
2026-01-30 18:34:25 +0100

    ajout headers CORS

--- next.config.js
+++ next.config.js
@@ -9,6 +9,19 @@ const nextConfig = {
     INVOCATION_API_URL: process.env.INVOCATION_API_URL || 'http://localhost:8084',
     COMBAT_API_URL: process.env.COMBAT_API_URL || 'http://localhost:8085',
   },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE,OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
